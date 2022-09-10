9594722268fc65d8d83e39e386396c9fa9c9e87c
JF 413 STUDIOS <79715830+jf413studios@users.noreply.github.com>
2022-09-10 16:38:06 -0400

    Working on about page.

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -1,71 +1,39 @@
 import './About.css';
 import Header from '../../components/Header/Header';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
-import { faCss3Alt } from '@fortawesome/free-brands-svg-icons';
-import { faJsSquare } from '@fortawesome/free-brands-svg-icons';
-import { faPhp } from '@fortawesome/free-brands-svg-icons';
-import { faShopify } from '@fortawesome/free-brands-svg-icons';
-import { faJava } from '@fortawesome/free-brands-svg-icons';
+
 
 function About(){
     return(
         <div className="about">
             <Header />
             <div className="page-content">
-                <div className="about-section-v1">
+                <section id="bio-banner">
                     <div className="container">
-                        <section id="bio">
-                            <div id="leftCol">
-                                <div id="profile-pic"></div>
-                            </div>
-                            <div id="rightCol">
-                                <h1>About</h1>
-                                <p>
-                                My name is <b>Jose Fuentes</b>.
-                                </p>
-                                <p>
-                                    I've been a developer for over 7 years. My passion is to create. Create something new that will bring a positive difference into this world.
-                                </p>
-                                <p>
-                                    When I'm not coding, I love spending time with my wonderful family. I’m a big marvel fan. I love to play video games. I like to keep myself in shape by working out. I play basketball, soccer, and volleyball. I’m also a Christian, so I love to spread the love of God wherever I go.
-                                </p>
-                                <a href="./Jose_Fuentes_Resume_Updated.pdf" class="cta" target="_blank" alt="Download Resume" rel="noopener noreferrer">Download Resume</a>
-                            </div>
-                        </section>
-                        <section id="skills">
-                            <h1>Skills</h1>
-                            <div id="skills-container">
-                                <div class="row">
-                                    <div class="skill-card">
-                                        <FontAwesomeIcon icon={faHtml5} />
-                                        <p>HTML</p>
-                                    </div>
-                                    <div class="skill-card">
-                                        <FontAwesomeIcon icon={faCss3Alt} />
-                                        <p>CSS</p>
-                                    </div>
-                                    <div class="skill-card">
-                                        <FontAwesomeIcon icon={faJsSquare} />
-                                        <p>JavaScript</p>
-                                    </div>
-                                    <div class="skill-card">
-                                        <FontAwesomeIcon icon={faPhp} />
-                                        <p>PHP</p>
-                                    </div>
-                                    <div class="skill-card">
-                                        <FontAwesomeIcon icon={faShopify} />
-                                        <p>Shopify</p>
-                                    </div>
-                                    <div class="skill-card">
-                                        <FontAwesomeIcon icon={faJava} />
-                                        <p>Java</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
+                        <h1>JOSE FUENTES</h1>
+                        <p>Front End Developer</p>
                     </div>
-                </div>
+                </section>
+                <section id="bio-description">
+                    <div className="container">
+                        <p>
+                            I've been a developer for over 7 years. My passion is to create. Create something new that will bring a positive difference into this world.
+                        </p>
+                        <p>
+                            When I'm not coding, I love spending time with my wonderful family. I’m a big marvel fan. I love to play video games. I like to keep myself in shape by working out. I play basketball, soccer, and volleyball. I’m also a Christian, so I love to spread the love of God wherever I go.
+                        </p>
+                        <a class="cta" target="_blank" href="/Jose_Fuentes_Resume_Updated.pdf">Download Resume</a>
+                    </div>
+                </section>
+                <section id="work-experience">
+                    <div className="container">
+                        <h3>My Work</h3>
+                        <h2>Experience</h2>
+                        <hr/>
+                        <div id="work-breakdown">
+
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     );
