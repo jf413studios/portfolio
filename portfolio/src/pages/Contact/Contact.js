a43e8e4ab9277adb4909de0a816694e6d883d12b
Samanta Carrizo <samantacarrizo@samantas-air.mynetworksettings.com>
2023-02-11 18:15:56 -0500

    Adding contact section from homepage to new stand alone contact page

--- portfolio/src/pages/Contact/Contact.js
+++ portfolio/src/pages/Contact/Contact.js
@@ -1,16 +1,17 @@
 import './Contact.css';
 import Header from '../../components/Header/Header';
+import Contact from '../Home/Sections/Contact/Contact';
 
 
-function Contact(){
+function ContactPage(){
     return(
         <div className="contact">
             <Header />
             <div className="page-content">
-                
+                <Contact />
             </div>
         </div>
     );
 }
 
-export default Contact;
+export default ContactPage;
