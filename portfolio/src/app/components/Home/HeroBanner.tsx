11fd1eb6870eff8ffd117c5f8ac51d34b88c8955
Jose Fuentes <jf413studios@gmail.com>
2025-05-10 20:38:54 -0400

    Removing the name jf 4:13 studios

--- portfolio/src/app/components/Home/HeroBanner.tsx
+++ portfolio/src/app/components/Home/HeroBanner.tsx
@@ -8,7 +8,7 @@ const HeroBanner = () => {
         <div className="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
             <div>
                 <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">Innovating the <span className="text-gold-500">Future</span>, One Product at a Time</h1>
-                <p className="mt-3 text-lg text-gray-800 dark:text-neutral-400">Welcome to JF 4:13 Studios. My name is Jose Fuentes, a front-end developer and entrepreneur with a passion for creating next-gen digital products. Focused on innovation, I combine sleek design and powerful code to build solutions that drive the future of technology.</p>
+                <p className="mt-3 text-lg text-gray-800 dark:text-neutral-400">Hi there! My name is Jose Fuentes, a front-end developer and entrepreneur with a passion for creating next-gen digital products. Focused on innovation, I combine sleek design and powerful code to build solutions that drive the future of technology.</p>
                 <div className="mt-7 grid gap-3 w-full sm:inline-flex">
                     <Link className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gold-500 text-woodsmoke-950 hover:bg-woodsmoke-950 hover:text-white disabled:opacity-50 disabled:pointer-events-none dark:hover:border dark:hover:border-gold-500" href="/work">
                         Discover My Work
